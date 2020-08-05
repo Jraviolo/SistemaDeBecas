cd77d8205b94528a0a1d81ddfd9b41efae94f636
NachoB18 <ignabertone@hotmail.com>
2020-08-05 01:58:58 -0300

    Dos consultas 2.0

--- SistemaDeBecas/src/main/java/SistemaDeBecas/Controller.java
+++ SistemaDeBecas/src/main/java/SistemaDeBecas/Controller.java
@@ -6,14 +6,30 @@ import com.complexible.stardog.api.SelectQuery;
 import com.stardog.stark.query.SelectQueryResult;
 
 public class Controller {
+
 	Connection connection = ConnectionConfiguration
 			.to("http://localhost:5820/DBPrueba")
 			.reasoning(true)
+			.database("SistemaBecasDB")
+			.credentials("admin","admin")
 			.connect()
 			.as(Connection.class);
-	
-	
-	SelectQuery query = connection.select("");
-    SelectQueryResult tupleQueryResult = query.execute();
+
+	SelectQuery alumnos = connection.select("SELECT ?persona WHERE { ?persona inscriptoACarrera: ?CarreraUTNFRSF .}");
+	SelectQuery resultadoAlumnos = alumnos.execute();
+	stringResultadoAlumnos = resultadoAlumnos.toString();
+
+	SelectQuery postulantes = connection.select("SELECT ?persona WHERE { ?persona inscriptoACarrera: ?CarreraUTNFRSF . ?persona tieneGrupoFamiliar: ."
+			+ "?persona tienePromedioGeneral: . ?persona inscriptoABeca: . FILTER NOT EXISTS{ ?persona tieneCargoUTNFRSF . ?persona tieneTitulo . } }");
+	SelectQuery resultadoPostulantes = postulantes.execute();
+	stringResultadoPostulantes = resultadoPostulantes.toString();
+
+	SelectQuery postulantesCalificados = connection.select();
+	SelectQuery resultadoPostulantesCalificados = postulantesCalificados.execute();
+	stringResultadoPostulantesCalificados = resultadoPostulantesCalificados.toString();
+
+	SelectQuery # = connection.select();
+	SelectQuery resultado# = #.execute();
+	stringResultado# = resultado#.toString();
     
 }
